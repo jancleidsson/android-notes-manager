bc04da79460e8290f21bbb4d5a590c659f536500
Jancleidsson Soares <jss@cesar.org.br>
2021-03-01 18:56:04 -0300

    Added initial projetc structure

---
+++ app/src/main/java/com/jss/notesmanager/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jss.notesmanager.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jss.notesmanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
