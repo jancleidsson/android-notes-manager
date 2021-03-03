6ec17b83f3ede4bcc9ef237fe8760eb86351864f
Jancleidsson Soares <jancleidsson@gmail.com>
2021-03-03 19:36:51 -0300

    Added navigation component

--- app/src/main/java/com/jss/notesmanager/presentation/NoteFragment.kt
+++ app/src/main/java/com/jss/notesmanager/presentation/NoteFragment.kt
@@ -5,17 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jss.notesmanager.R
+import androidx.navigation.Navigation
+import com.jss.notesmanager.databinding.FragmentNoteBinding
 
 /**
- * A simple [Fragment] subclass.
- * Use the [NoteFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Represents note fragment view
  */
 class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/jss/notesmanager/presentation/NotesListFragment.kt
+++ app/src/main/java/com/jss/notesmanager/presentation/NotesListFragment.kt
@@ -5,18 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import com.jss.notesmanager.databinding.FragmentNotesListBinding
+import androidx.navigation.Navigation
 import com.jss.notesmanager.R
 
 /**
- * A simple [Fragment] subclass.
- * Use the [NotesListFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Represents notes list fragment view
  */
 class NotesListFragment : Fragment() {
+
+    private var _binding: FragmentNotesListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+        _binding = FragmentNotesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.createNotesBtn.setOnClickListener { goToNoteDetails() }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action: NavDirections = NotesListFragmentDirections.actionNotesListToNote()
+        Navigation.findNavController(binding.notesListView).navigate(action)
     }
 }
