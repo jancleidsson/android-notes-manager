27d5849442237120d126d9c864e36283831dfb04
Jancleidsson Soares <jancleidsson@gmail.com>
2021-03-03 20:16:01 -0300

    Added note entity using room

---
+++ app/src/main/java/com/jss/notesmanager/framework/db/NoteEntity.kt
@@ -0,0 +1,27 @@
+package com.jss.notesmanager.framework.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.jss.core.data.Note
+
+@Entity(tableName = "note")
+data class NoteEntity (
+
+    val title: String,
+    val content: String,
+
+    @ColumnInfo(name = "creation_date")
+    val creationTime: Long,
+
+    @ColumnInfo(name = "update_time")
+    val updateTime: Long,
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0
+){
+    companion object {
+        fun fromNote(note: Note) = NoteEntity(note.title, note.content, note.creationTime, note.updateTime)
+    }
+    fun toNote() = Note(title, content, creationTime, updateTime)
+}
