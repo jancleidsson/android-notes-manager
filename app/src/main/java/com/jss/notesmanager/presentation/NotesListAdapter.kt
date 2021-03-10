d918e865f8de42af79f64dccba8c3818b002dfeb
Jancleidsson Soares <jancleidsson@gmail.com>
2021-03-10 20:53:24 -0300

    Added note item layout and notes list adapter.

---
+++ app/src/main/java/com/jss/notesmanager/presentation/NotesListAdapter.kt
@@ -0,0 +1,42 @@
+package com.jss.notesmanager.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jss.core.data.Note
+import com.jss.notesmanager.R
+import com.jss.notesmanager.databinding.NoteItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class NotesListAdapter(private val notes: ArrayList<Note>) : RecyclerView.Adapter<NotesListAdapter.NoteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = NoteViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false))
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(notes[position])
+    }
+
+    override fun getItemCount() = notes.size
+
+    fun updateNotes(newNotes: List<Note>) {
+        notes.clear()
+        notes.addAll(notes)
+        notifyDataSetChanged()
+    }
+
+    inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = NoteItemBinding.bind(view)
+        private val context = view.context
+
+        fun bind(note: Note) {
+            val lastUpdateFormat = SimpleDateFormat(context.getString(R.string.last_updated_date_format))
+            val lastUpdateDate = Date(note.updateTime)
+            binding.noteItemLastUpdated.text = "${context.getString(R.string.last_updated)} ${lastUpdateFormat.format(lastUpdateDate)}"
+            binding.noteItemTitle.text = note.title
+            binding.noteItemContent.text = note.content
+        }
+    }
+}
